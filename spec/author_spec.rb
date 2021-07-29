18255f013d531c8db97b57b22a916707f406147a
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-29 13:44:56 -0600

    Add write test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -19,4 +19,15 @@ RSpec.describe Author do
 
     expect(charlotte_bronte.books).to eq([])
   end
+
+  it "can write a new book" do
+    # Get an author
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    # Write a new book
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    expect(jane_eyre).to be_a(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+  end
 end
