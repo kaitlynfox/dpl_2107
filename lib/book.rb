e77b58caa448d1e5c5e457b8506f1b95e8762af1
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-29 13:46:49 -0600

    Add publication_date instance variable and attr_reader

--- lib/book.rb
+++ lib/book.rb
@@ -2,15 +2,13 @@ class Book
 
   attr_reader :author,
               :title,
+              :publication_date,
               :publication_year
 
   def initialize(hash)
     @author = hash[:author_first_name] + " " + hash[:author_last_name]
     @title = hash[:title]
-    # don't need to access or get in a method
-    publication_date = hash[:publication_date]
-    # need to access this and get in a method
-    # get range of the string
+    @publication_date = hash[:publication_date]
     @publication_year = publication_date[(publication_date.length-4)..(publication_date.length-1)]
   end
 
