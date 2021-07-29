e617db6c2195a66099bd471d0caa4c847eb1510d
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-29 14:13:25 -0600

    Add add_author tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,5 +1,6 @@
 require './lib/library'
 require './lib/author'
+require './lib/book'
 
 RSpec.describe Library do
   it "exists" do
@@ -24,5 +25,27 @@ RSpec.describe Library do
     dpl = Library.new("Denver Public Library")
 
     expect(dpl.authors).to eq([])
+  end
+
+  it "can add authors to a library" do
+    # Get a library
+    dpl = Library.new("Denver Public Library")
+
+    # Get authors
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    # Get books
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    # Add authors to the library's list of authors
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.authors).to eq([charlotte_bronte, harper_lee])
+    expect(dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
   end
 end
