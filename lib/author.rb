d1e78c4e850baf2c4c7e3eb044ce5ebd7b936859
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-29 13:46:09 -0600

    Add write method

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,23 @@
 class Author
 
-  attr_reader :name,
-              :books 
+  attr_reader :first_name,
+              :last_name,
+              :name,
+              :books
 
   def initialize(hash)
+    @first_name = hash[:first_name]
+    @last_name = hash[:last_name]
     @name = hash[:first_name] + " " + hash[:last_name]
     @books = []
+  end
+
+  def write(title, publication_date)
+    # Author writes a new book
+    book = Book.new({author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date})
+    # The book is saved to their list of books written 
+    @books << book
+    #return a single book object
+    book
   end
 end
