263ee961d52a63e110c8a68d111915a3d04f1c42
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-29 14:56:55 -0600

    Add checkout test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -71,4 +71,25 @@ RSpec.describe Library do
     expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
     expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
   end
+
+
+
+
+  # Iteration 4
+  it "can checkout a book" do
+    # Get a library
+    dpl = Library.new("Denver Public Library")
+
+    # Get authors
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    # Get books
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    # Can't be checkout because it currently doesn't exist in library
+    expect(dpl.checkout(mockingbird)).to eq(false)
+  end
 end
